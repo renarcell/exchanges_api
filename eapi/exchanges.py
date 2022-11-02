a298130202c82a0f73ee139444a29c544ea0cc8a
Alex <renarcellx@gmail.com>
2022-11-02 17:01:47 +0200

    move get_all_exchanges to exchanges

---
+++ eapi/exchanges.py
@@ -0,0 +1,16 @@
+from .FtxExchange import FtxExchange
+from .FtxExchange import FtxExchange
+from .MexcExchange import MexcExchange
+from .ExmoExchange import ExmoExchange
+from .BitMartExchange import BitMartExchange
+from .GateIoExchange import GateIoExchange
+
+
+def get_all_exchanges():
+    return [
+        FtxExchange(),
+        MexcExchange(),
+        ExmoExchange(),
+        BitMartExchange(),
+        GateIoExchange(),
+    ]
--- eapi/utils.py
+++ eapi/utils.py
@@ -1,22 +1,6 @@
 from urllib.parse import urljoin
 from .exceptions import ServerError, ClientError, ParseError
 
-from .FtxExchange import FtxExchange
-from .MexcExchange import MexcExchange
-from .ExmoExchange import ExmoExchange
-from .BitMartExchange import BitMartExchange
-from .GateIoExchange import GateIoExchange
-
-
-def get_all_exchanges():
-    return [
-        FtxExchange(),
-        MexcExchange(),
-        ExmoExchange(),
-        BitMartExchange(),
-        GateIoExchange(),
-    ]
-
 
 class BaseUrl:
     def __init__(self, base_url):
